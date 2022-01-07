3d7767337b0c895c71a6419509fdfc8e8f8a5451
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2022-01-07 18:22:30 +0700

    add main and has_input function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,16 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
 
-  @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
   end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5,input)
+    |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
+
+
 end
