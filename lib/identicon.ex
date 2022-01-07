78260a7e1b1c23405946c6b3aa75e1321c7dda1c
Andri Yabu Ndapa Rehing <andriyabu@gmail.com>
2022-01-07 19:27:02 +0700

    add pic_color function to get 3 element of struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,8 +4,19 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pic_color
   end
 
+  def pic_color(image) do
+    # first way
+    # %Identicon.Image{hex: hex_list} = image
+    # [r,g,b | _tail] = hex_list
+    # second way, the short one
+    %Identicon.Image{hex: [r,g,b | _tail]} = image
+    [r,g,b]
+  end
+
+  @spec hash_input(any) :: %Identicon.Image{hex: [byte]}
   def hash_input(input) do
     hex = :crypto.hash(:md5,input)
     |> :binary.bin_to_list
